4944c16698b7a31faa966947c00c35ef72947d48
yinyinUMSI <yinyin@umich.edu>
2019-10-15 15:44:16 -0400

    add 8

--- jsOutput.js
+++ jsOutput.js
@@ -2,6 +2,8 @@ function showResult() {
 	var inputContent = document.getElementById("input").value;
 	// console.log(inputContent);
 	// console.log(typeof inputContent);
+	var N = parseInt(document.getElementById('selectValue').value);
+	console.log(N);
 	var strArray = inputContent.split(',');
 	var count = 0;
 	var finalCombined = [];
@@ -19,15 +21,15 @@ function showResult() {
 		count += 1;
 	}
 
-	var OneLine = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+	var OneLine = new Array(N).fill(0);
 	var adaptWhole = [];
-	for (i=0; i< 16; i++) {
-		for (j = 0; j<16; j++) {
+	for (i=0; i< N; i++) {
+		for (j = 0; j<N; j++) {
 			if (i%2 == 0) {
-				OneLine[15-j] = finalCombined[i*16+j];
+				OneLine[N-1-j] = finalCombined[i*N+j];
 			}
 			else {
-				OneLine[j] = finalCombined[i*16+j];
+				OneLine[j] = finalCombined[i*N+j];
 			}
 		}
 		adaptWhole.push(OneLine);
